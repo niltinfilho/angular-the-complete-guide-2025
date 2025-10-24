540f10e1a68c05ec314a62b5008c377efa9ee6e6
niltinfilho <niltonfilho115@gmail.com>
2025-10-24 14:22:11 -0300

    Defining Component Inputs

--- essentials/src/app/user/user.component.ts
+++ essentials/src/app/user/user.component.ts
@@ -1,22 +1,18 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onSelectUser() { }
 }
