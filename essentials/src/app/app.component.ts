5b813fad5eb970c35f75e2e5300af5b08f211d60
niltinfilho <niltonfilho115@gmail.com>
2025-10-27 08:49:16 -0300

    Working with Outputs & Emitting Data

--- essentials/src/app/app.component.ts
+++ essentials/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +12,8 @@ import { DUMMY_USERS } from './dummy-users';
 })
 export class AppComponent {
   users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('Selected user with id: ' + id);
+  }
 }
