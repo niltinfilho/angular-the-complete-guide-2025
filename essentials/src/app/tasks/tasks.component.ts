ad7b28a6309fa287bc8cd933d979d34734f8c1f2
niltinfilho <niltonfilho115@gmail.com>
2025-10-27 09:39:56 -0300

    TypeScript: Working With Potentially Undefined Values & Union Types

--- essentials/src/app/tasks/tasks.component.ts
+++ essentials/src/app/tasks/tasks.component.ts
@@ -9,5 +9,5 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input({ required: true }) name!: string;
+  @Input() name?: string;
 }
