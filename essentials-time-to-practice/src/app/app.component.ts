36569e21532dd2865eb8a578431df7d3b65fb2a0
niltinfilho <niltonfilho115@gmail.com>
2025-10-28 09:35:37 -0300

    got de right starting project

--- essentials-time-to-practice/src/app/app.component.ts
+++ essentials-time-to-practice/src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'essentials-time-to-practice';
-}
+export class AppComponent { }
