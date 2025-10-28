927105f05320952015f12b9e52edcf43a7f1f323
niltinfilho <niltonfilho115@gmail.com>
2025-10-28 11:56:45 -0300

    Migrating to Angular Modules

--- essentials-time-to-practice/src/app/investment-results/investment-results.component.ts
+++ essentials-time-to-practice/src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, computed, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
